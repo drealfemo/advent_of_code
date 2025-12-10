92bf666968f856c39eceeab8b7f23b4606339d34
Gideon Adekunle <gideonf.adekunle@gamil.com>
2025-12-10 20:10:01 +0000

    day 7 solution updated

--- aoc_2025/day_07.py
+++ aoc_2025/day_07.py
@@ -1,9 +1,11 @@
 from functools import lru_cache
 
 
-def count_timelines(grid):
+def count_splits_and_timelines(grid):
+    splits = 0
     @lru_cache(maxsize=None)
     def dfs(current_position):
+        nonlocal splits
         row, col = current_position
         if not (0 <= row < len(grid) and 0 <= col < len(grid[0])):
             return 1
@@ -12,10 +14,11 @@ def count_timelines(grid):
         if value in (".", "S"):
             return dfs((row + 1, col))
         elif value == "^":
+            splits += 1
             return dfs((row, col - 1)) + dfs((row, col + 1))
         return 1
 
-    return dfs((1, grid[0].index("S")))
+    return dfs((1, grid[0].index("S"))), splits
 
 
 def count_splits(grid):
@@ -49,5 +52,5 @@ if __name__ == "__main__":
     with open("data/input_day07.txt") as f:
         data = f.read().splitlines()
 
-    print(count_splits(data))
-    print(count_timelines(data))
+    print(count_splits(data))  # part 1 only
+    print(*count_splits_and_timelines(data))  # part 1 and 2
